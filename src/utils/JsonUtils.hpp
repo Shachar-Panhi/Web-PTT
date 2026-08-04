a27bb960961e98109eff77c6923fb41d1bdb2de1
shachar-Panhi <.>
2026-08-04 15:12:14 +0300

    Feat - added JsonUtils

---
+++ src/utils/JsonUtils.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <glaze/core/context.hpp>
+#include <glaze/glaze.hpp>
+#include <boost/system/error_code.hpp>
+#include <expected>
+#include <string>
+
+namespace JsonUtils {
+
+template <typename T>
+std::expected<T, glz::error_ctx> parse_json(const std::string& json_str) {
+    T obj;
+    glz::error_ctx errc = glz::read_json(obj, json_str);
+    if (errc) {
+        return std::unexpected(errc);
+    }
+    return obj;
+}
+
+template <typename T>
+std::expected<std::string, glz::error_ctx> serialize_json(const T& obj) {
+    std::string json_str;
+    glz::error_ctx errc = glz::write_json(obj, json_str);
+    if (errc) {
+        return std::unexpected(errc);
+    }
+    return json_str;
+}
+
+} // namespace JsonUtils
