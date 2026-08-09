e34e2e8fd3dc7a9c59bd91924d50163b8be3ea44
shachar-Panhi <.>
2026-08-09 09:55:04 +0300

    Refactor - added template for sending response and organised functions

--- src/api/Session.cpp
+++ src/api/Session.cpp
@@ -1,8 +1,9 @@
 #include "Session.hpp"
 #include "../utils/JsonUtils.hpp"
 #include "Types.hpp"
+#include <glaze/glaze.hpp>
+#include <rtc/rtc.hpp>
 #include "boost/beast/http/message_fwd.hpp"
-
 
 namespace WebPTT::Api { 
     Session::Session(tcp::socket socket)
@@ -31,124 +32,83 @@ namespace WebPTT::Api {
                 }
                 break;
             }
-            std::expected<HTTP::response<HTTP::string_body>, HTTP::response<HTTP::file_body>> res = handle_request(req);
-            
-            bool need_eof = false;
-            if (res) {
-                need_eof = res->need_eof();
-                co_await send_response(*res);
-            } else {
-                need_eof = res.error().need_eof();
-                co_await send_response_filebody(res.error());
-            }
-
-            if (need_eof) {
-                errc = stream_.socket().shutdown(tcp::socket::shutdown_send, errc);
-                break;
-            }
+            co_await handle_request(std::move(req));
         }
     }
 
-    std::expected<HTTP::response<HTTP::string_body>, HTTP::response<HTTP::file_body>> Session::handle_request (const HTTP::request<HTTP::string_body>& req) {
-        HTTP::response<HTTP::string_body> res;
-        std::expected<HTTP::response<HTTP::file_body>, HTTP::response<HTTP::string_body>> file_res;
+    boost::asio::awaitable<void> Session::handle_request(HTTP::request<HTTP::string_body> req) {
 
         if (req.target() == "/isAlive" && req.method() == HTTP::verb::get) {
-            res = handle_is_alive(req);
+            co_await send_response(handle_is_alive(req));
         }
         else if (req.target() == "/ptt/start" && req.method() == HTTP::verb::post) {
-            res = handle_ptt_start(req);
+            co_await send_response(handle_ptt_start(req));
         }
         else if (req.target() == "/ptt/stop" && req.method() == HTTP::verb::post) {
-            file_res = handle_ptt_stop(req);
+            auto file_res = handle_ptt_stop(req);
             if (file_res) {
-                return std::unexpected(std::move(*file_res));
+                co_await send_response(std::move(*file_res));
+            } else {
+                co_await send_response(std::move(file_res.error()));
             }
-            res = file_res.error();
         }
         else {
-            res.set(HTTP::field::content_type, "application/json");
-            res.result(HTTP::status::not_found);
-            res.body() = R"({"status": "not found"})";
+            co_await send_response(make_error_response(HTTP::status::not_found, req.version(), R"({"status": "not found"})"));
         }
-
-        res.prepare_payload();
-
-        return std::move(res);
-    }
-    
-    boost::asio::awaitable<void> Session::send_response(HTTP::response<HTTP::string_body>& res) {
-        boost::system::error_code errc;                        
-        co_await HTTP::async_write(stream_, res, boost::asio::redirect_error(boost::asio::use_awaitable, errc));
-            if (errc) {
-                spdlog::error("write error: {}", errc.message());
-                co_return;
-            }
     }
 
-    boost::asio::awaitable<void> Session::send_response_filebody(HTTP::response<HTTP::file_body>& res) {
-        boost::system::error_code errc; 
-        co_await HTTP::async_write(stream_, res, boost::asio::redirect_error(boost::asio::use_awaitable, errc));
-        if (errc) {
-            spdlog::error("write error: {}", errc.message());
-            co_return;
-        }
+    HTTP::response<HTTP::string_body> Session::make_api_response(HTTP::status status, unsigned int version, const std::string& body_data) {
+        HTTP::response<HTTP::string_body> res{status, version};
+        res.set(HTTP::field::content_type, "application/json");
+        res.body() = body_data;
+        return res;
+    }
+
+    HTTP::response<HTTP::string_body> Session::make_error_response(HTTP::status status, unsigned int version, const std::string& body_data) {
+        HTTP::response<HTTP::string_body> res{status, version};
+        res.set(HTTP::field::content_type, "application/json");
+        res.body() = body_data;
+        return res;
     }
 
     HTTP::response<HTTP::string_body> Session::handle_is_alive(const HTTP::request<HTTP::string_body>& req) {
-        HTTP::response<HTTP::string_body> res{HTTP::status::ok, req.version()};
-        res.set(HTTP::field::content_type, "application/json");
-        res.result(HTTP::status::ok);
-        res.body() = R"({"message": "alive", "status": "ok"})";
-        return res;
+        return make_api_response(HTTP::status::ok, req.version(), R"({"message": "alive", "status": "ok"})");
     }
 
     HTTP::response<HTTP::string_body> Session::handle_ptt_start(const HTTP::request<HTTP::string_body>& req) {
-        HTTP::response<HTTP::string_body> res{HTTP::status::ok, req.version()};
-        
         auto result = WebPTT::Utils::parse_json<MessageBody>(req.body());
+        
         if (result) {
             const MessageBody& body = result.value();
             spdlog::info("Received message: {}, status: {}", body.message_, body.status_);
-            res.set(HTTP::field::content_type, "application/json");
-            res.result(HTTP::status::ok);
-            res.body() = R"({"message": "received", "status": "ok"})";
-        } else {
-            std::string glz_error = glz::format_error(result.error());
-            spdlog::error("Failed to parse request body: {}", glz_error);
-            ErrorResponse error_response{.message_ = std::move(glz_error), .status_ = "error"};
+            return make_api_response(HTTP::status::ok, req.version(), R"({"message": "received", "status": "ok"})");
+        }  
+        std::string glz_error = glz::format_error(result.error());
+        spdlog::error("Failed to parse request body: {}", glz_error);
+        
+        ErrorResponse error_response{.message_ = std::move(glz_error), .status_ = "error"};
+        auto result_json = WebPTT::Utils::serialize_json(error_response);
             
-            res.set(HTTP::field::content_type, "application/json");
-            auto result_json = WebPTT::Utils::serialize_json(error_response);
-            if (result_json) {
-                res.result(HTTP::status::bad_request);
-                res.body() = result_json.value();
-            } else {
-                    res.result(HTTP::status::internal_server_error);
-                    res.body() = R"({"message": "Failed to serialize error response", "status": "error"})";
-            }
-        }
-        return res;
+        if (result_json) {
+            return make_error_response(HTTP::status::bad_request, req.version(), result_json.value());
+        }  
+        return make_error_response(HTTP::status::internal_server_error, req.version(), R"({"message": "Failed to serialize error response", "status": "error"})");
     }   
     
     std::expected<HTTP::response<HTTP::file_body>, HTTP::response<HTTP::string_body>> Session::handle_ptt_stop(const HTTP::request<HTTP::string_body>& req) {
         boost::beast::error_code errc;
         HTTP::file_body::value_type body;
 
-
         body.open("src/dummy.wav", boost::beast::file_mode::read, errc);
+        
         if (errc) {
             spdlog::error("Failed to open file: {}", errc.message());
-            HTTP::response<HTTP::string_body> res{HTTP::status::ok, req.version()};
-            res.result(HTTP::status::internal_server_error);
-            res.set(HTTP::field::content_type, "application/json");
-            res.body() = R"({"message": "Failed to open file", "status": "error"})";
-            return std::unexpected(res);
+            return std::unexpected(make_error_response(HTTP::status::internal_server_error, req.version(), R"({"message": "Failed to open file", "status": "error"})"));
         }
         
         HTTP::response<HTTP::file_body> file_res{std::piecewise_construct, std::make_tuple(std::move(body)), std::make_tuple(HTTP::status::ok, req.version())};        
         file_res.set(HTTP::field::content_type, "audio/wav");
 
-        return std::move(file_res);
+        return file_res;
     }   
 }  // namespace WebPTT::Api
--- src/api/Session.hpp
+++ src/api/Session.hpp
@@ -2,8 +2,6 @@
 
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
-#include <glaze/glaze.hpp>
-#include <rtc/rtc.hpp>
 #include "boost/asio/any_io_executor.hpp"
 #include <boost/beast/http.hpp>
 #include <spdlog/spdlog.h>
@@ -17,12 +15,23 @@ namespace WebPTT::Api {
     public: 
         explicit Session(tcp::socket);
         boost::asio::awaitable<void> start();
-        static std::expected<HTTP::response<HTTP::string_body>, HTTP::response<HTTP::file_body>> handle_request(const HTTP::request<HTTP::string_body>& req);
-        boost::asio::awaitable<void> send_response(HTTP::response<HTTP::string_body>& res);
-        boost::asio::awaitable<void> send_response_filebody(HTTP::response<HTTP::file_body>& res);
+        boost::asio::awaitable<void> handle_request(HTTP::request<HTTP::string_body> req);
+        static HTTP::response<HTTP::string_body> make_api_response(HTTP::status status, unsigned int version, const std::string& body_data);
+        static HTTP::response<HTTP::string_body> make_error_response(HTTP::status status, unsigned int version, const std::string& body_data);
         static HTTP::response<HTTP::string_body> handle_is_alive(const HTTP::request<HTTP::string_body>& req);
         static HTTP::response<HTTP::string_body> handle_ptt_start(const HTTP::request<HTTP::string_body>& req);
         static std::expected<HTTP::response<HTTP::file_body>, HTTP::response<HTTP::string_body>> handle_ptt_stop(const HTTP::request<HTTP::string_body>& req);
+
+        template <typename T>
+        boost::asio::awaitable<void> send_response(HTTP::response<T> res) {
+            boost::system::error_code errc;                        
+            res.prepare_payload();
+            co_await HTTP::async_write(stream_, res, boost::asio::redirect_error(boost::asio::use_awaitable, errc));
+            if (errc) {
+                spdlog::error("write error: {}", errc.message());
+                co_return;
+            }
+        }
 
 
     private:
